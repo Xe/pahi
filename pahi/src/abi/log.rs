ae85f026f8e5d41aa2c9806421f6b3dc58b7597c
Christine Dodrill <me@christine.website>
2020-02-19 19:27:06 +0000

    pa'i/abi/log: println! instead of <level>!

--- pahi/src/abi/log.rs
+++ pahi/src/abi/log.rs
@@ -1,5 +1,4 @@
 use crate::env::*;
-use log::*;
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 use wasmer_runtime::{Array, Ctx, WasmPtr};
@@ -7,22 +6,20 @@ use wasmer_runtime::{Array, Ctx, WasmPtr};
 #[repr(u32)]
 #[derive(Debug, FromPrimitive)]
 pub enum LogLevel {
-  Error = 1,
-  Warning = 3,
-  Info = 6,
+    Error = 1,
+    Warning = 3,
+    Info = 6,
 }
 
 pub fn write(ctx: &mut Ctx, level: u32, ptr: WasmPtr<u8, Array>, len: u32) {
-  let (memory, env) = OlinEnv::get_memory_and_environment(ctx, 0);
-  let string = ptr.get_utf8_string(memory, len).unwrap();
-  match FromPrimitive::from_u32(level) {
-    Some(LogLevel::Info) => info!("{}: {}", env.host_name, string),
-    Some(LogLevel::Warning) => warn!("{}: {}", env.host_name, string),
-    Some(LogLevel::Error) => error!("{}: {}", env.host_name, string),
-    None => {
-      panic!("invalid log level {}", level);
+    let (memory, env) = OlinEnv::get_memory_and_environment(ctx, 0);
+    let string = ptr.get_utf8_string(memory, len).unwrap();
+    match FromPrimitive::from_u32(level) {
+        Some(LogLevel::Info) => println!("{}: info: {}", env.host_name, string),
+        Some(LogLevel::Warning) => println!("{}: warn: {}", env.host_name, string),
+        Some(LogLevel::Error) => println!("{}: error: {}", env.host_name, string),
+        None => panic!("invalid log level {}", level),
     }
-  }
 
-  env.log_call("log_write".to_string());
+    env.log_call("log_write".to_string());
 }
