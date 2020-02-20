c12473a60e917e5a83116f131062fa76893da8b0
Christine Dodrill <me@christine.website>
2020-02-20 12:34:31 +0000

    oops

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let
   sources = import ./nix/sources.nix;
   niv = import sources.niv { };
   pkgs = import sources.nixpkgs { };
-  olin = import sources.olin { inherit sources pkgs; };
+  olin = import sources.olin { inherit pkgs; };
   rust = import ./nix/rust.nix { inherit sources; };
   dhall = import ./nix/dhall.nix;
 in pkgs.mkShell {
