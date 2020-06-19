37a458edf8ae16ddddd11511f9daf82e0091c64e
Christine Dodrill <me@christine.website>
2020-06-19 11:43:46 -0400

    bump rust version

--- nix/rust.nix
+++ nix/rust.nix
@@ -4,6 +4,6 @@ let
   pkgs =
     import sources.nixpkgs { overlays = [ (import sources.nixpkgs-mozilla) ]; };
   channel = "nightly";
-  date = "2020-03-19";
+  date = "2020-06-10";
   targets = [ "wasm32-unknown-unknown" "wasm32-wasi" ];
 in pkgs.rustChannelOfTargets channel date targets
